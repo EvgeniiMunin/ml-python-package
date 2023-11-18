cdb3eeadbb6a511e38ab574a831be92c71c6cd0f
EvgeniiMunin <35193823+EvgeniiMunin@users.noreply.github.com>
2023-11-18 23:07:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name="ml_python_package",
+    name="ml_python_package_evgenii",
     packages=find_packages(),
     version="0.1.1",
     description="Training stuff",
