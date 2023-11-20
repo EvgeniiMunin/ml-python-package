daa85b7ee57c576751b449c7521219f8f8953855
EvgeniiMunin <35193823+EvgeniiMunin@users.noreply.github.com>
2023-11-21 00:35:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name="ml_python_package_evgenii",
     packages=find_packages(),
-    version="0.1.7",
+    version="0.1.;",
     description="Training stuff",
     author="Evgenii",
     install_requires=["scikit-learn==0.24.1", "pandas==1.1.5",],
